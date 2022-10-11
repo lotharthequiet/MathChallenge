a0203bd16c2ac8a8484226cc452e486f90d279d0
Lothar TheQuiet <lotharthequiet@gmail.com>
2022-10-10 23:14:46 -0400

    Lothar updated MathChallenge.cpp

--- MathChallenge.cpp
+++ MathChallenge.cpp
@@ -2,6 +2,41 @@
 #include <cmath>
 using namespace std;
 
+void print_title(){
+    cout << "MathChallenge - Python Edition" << endl;
+    cout << "------------------------------" << endl;
+    cout << "" << endl;
+}
+
+void print_div(){
+    cout << "------------------------------" << endl;
+}
+
+int addition(int x, int y){
+    return (x + y);
+}
+
+int subtraction(int x, int y){
+    return (x - y);
+}
+
+int multiplication(int x, int y){
+    return (x * y);
+}
+
+int division(int x, int y){
+    return (x / y);
+}
+
+double exponent(int x, int y){
+    static_cast<double>(x);
+    static_cast<double>(y);
+    return (pow(x, y));
+}
+
 int main(){
-    cout << "Testes." << endl;
+    print_title();
+
+
+    return 0;
 }
