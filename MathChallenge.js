cc49d7d3bea23de1480b058047975d595597f298
Lothar TheQuiet <lotharthequiet@gmail.com>
2022-10-13 23:41:20 -0400

    Lothar updated MathChallenge-JavaScript Edition

--- MathChallenge.js
+++ MathChallenge.js
@@ -1,2 +1,27 @@
 "use strict";
-alert("Testing.")
+
+function print_title(){
+    console.log('MathChallenge - JavaScript Edition')
+    console.log('----------------------------------')
+    console.log('')
+}
+
+function print_div(){
+    console.log('------------------------------')
+}
+
+function addition(x,y){
+    return x + y
+}
+function subtraction(x,y){
+    return x - y
+}
+function multiplication(x,y){
+    z = x * y
+    return z
+}
+function division(x,y){
+    return x / y
+}
+
+print_title();
