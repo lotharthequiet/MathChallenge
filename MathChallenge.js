02082447858ae167f3bba0b00301adbd52f829d6
Lothar TheQuiet <lotharthequiet@gmail.com>
2022-10-14 22:46:55 -0400

    Lothar updated MathChallenge.js

--- MathChallenge.js
+++ MathChallenge.js
@@ -26,6 +26,14 @@ function division(x,y){
     return x / y;
 }
 
+function exponent(x,y){
+    return x**y
+}
+
+function orderofops(u,v,w,x,y){
+    return u-v*(w/x)*y+x/x
+}
+
 print_title();
 console.log('ADDITION: 12+37 = 49');
 console.log('Addition: 12+37 =', addition(12,37),'*');
@@ -38,4 +46,10 @@ console.log('Multiplication: 23 * 45 =', multiplication(23,45),'*');
 print_div();
 console.log('DIVISION: 152400 / 20 = 7620');
 console.log('Division: 152400 / 20 =', division(152400,20),'*');
+print_div();
+console.log('EXPONENT - 152^3 = 3,511,808');
+console.log('Exponent - 152^3 =', exponent(152,3),'*');
+print_div();
+console.log('ORDER OF OPERATIONS: 500-4(6/3)*17+3/3 = 365');
+console.log('Order of Operations: 500-4(6/3)*17+3/3 =', orderofops(500,4,6,3,17),'*');
 print_div();
